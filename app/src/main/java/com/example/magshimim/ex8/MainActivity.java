b4dbd99c44d392aefc9532b11ccbad24fdd22b6a
gon <gon.galili5@gmail.com>
2018-02-03 12:52:18 +0200

    3.2.18

--- app/src/main/java/com/example/magshimim/ex8/MainActivity.java
+++ app/src/main/java/com/example/magshimim/ex8/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.magshimim.ex8;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +10,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+import javax.xml.transform.Result;
 
 public class MainActivity extends Activity {
     EditText field1;
     EditText field2;
     Button go;
+    final int CHECK = 1;
     RadioButton rCa;
     RadioButton rCh;
     @Override
@@ -24,7 +28,7 @@ public class MainActivity extends Activity {
         field1 = (EditText)findViewById(R.id.input1);
         field2 = (EditText)findViewById(R.id.input2);
         rCa = (RadioButton) findViewById(R.id.calc);
-        rCh = (RadioButton) findViewById(R.id.check);
+        rCh = (RadioButton) findViewById(R.id.right);
         go = (Button) findViewById(R.id.bGo);
         go.setEnabled(false);
         field1.setEnabled(false);
@@ -39,21 +43,21 @@ public class MainActivity extends Activity {
 
             public void onTextChanged(CharSequence s, int start, int before, int count)
             {
-                if(rCa.isChecked() && field1.getText().toString().length() >0)
+                if(rCa != null && rCa.isChecked() && field1.getText().toString().length() >0)
                 {
                     field2.setEnabled(false);
                     go.setEnabled(true);
                 }
-                else if(rCa.isChecked() && field1.getText().toString().length() == 0)
+                else if(rCa != null && rCa.isChecked() && field1.getText().toString().length() == 0)
                 {
                     field2.setEnabled(true);
                     go.setEnabled(false);
                 }
-                else if(rCh.isChecked()  && field1.getText().toString().length() >0 && field2.getText().toString().length() >0)
+                else if(rCh != null && rCh.isChecked()  && field1.getText().toString().length() >0 && field2.getText().toString().length() >0)
                 {
                     go.setEnabled(true);
                 }
-                else if(rCh.isChecked()  && (field1.getText().toString().length() ==0 || field2.getText().toString().length() ==0))
+                else if(rCh != null && rCh.isChecked()  && (field1.getText().toString().length() ==0 || field2.getText().toString().length() ==0))
                 {
                     go.setEnabled(false);
                 }
@@ -74,21 +78,21 @@ public class MainActivity extends Activity {
 
             public void onTextChanged(CharSequence s, int start, int before, int count)
             {
-                if(rCa.isChecked() && field2.getText().toString().length() >0)
+                if(rCa != null && rCa.isChecked() && field2.getText().toString().length() >0)
                 {
                     field1.setEnabled(false);
                     go.setEnabled(true);
                 }
-                else if(rCa.isChecked() && field1.getText().toString().length() == 0)
+                else if(rCa != null && rCa.isChecked() && field1.getText().toString().length() == 0)
                 {
                     field1.setEnabled(true);
                     go.setEnabled(false);
                 }
-                else if(rCh.isChecked()  && field1.getText().toString().length() >0 && field2.getText().toString().length() >0)
+                else if(rCh != null && rCh.isChecked()  && field1.getText().toString().length() >0 && field2.getText().toString().length() >0)
                 {
                     go.setEnabled(true);
                 }
-                else if(rCh.isChecked()  && (field1.getText().toString().length() ==0 || field2.getText().toString().length() ==0))
+                else if(rCh != null && rCh.isChecked()  && (field1.getText().toString().length() ==0 || field2.getText().toString().length() ==0))
                 {
                     go.setEnabled(false);
                 }
@@ -114,5 +118,59 @@ public class MainActivity extends Activity {
             field2.setEnabled(false);
         }
     }
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, ResultActivity.class);
+        EditText editText1 = (EditText) findViewById(R.id.input1);
+        EditText editText2 = (EditText) findViewById(R.id.input2);
+        if(editText1.getText() != null) {
+            String val1 = editText1.getText().toString();
+            intent.putExtra("far", val1);
+        }
+        else
+        {
+            intent.putExtra("far", "");
+        }
+        if(editText1.getText() != null) {
+            String val2 = editText2.getText().toString();
+            intent.putExtra("cel", val2);
+        }
+        else
+        {
+            intent.putExtra("cel", "");
+        }
 
+
+
+
+        if(rCh.isChecked())
+        {
+            intent.putExtra("check", "rch");
+        }
+        else
+        {
+            intent.putExtra("check", "rca");
+        }
+        startActivityForResult(intent, CHECK);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(!rCh.isChecked()) {
+            EditText editText;
+            if (data.getStringExtra("empty").compareTo("cel") == 0) {
+                editText = (EditText) findViewById(R.id.input2);
+            } else {
+                editText = (EditText) findViewById(R.id.input1);
+            }
+
+            if (requestCode == CHECK) {
+                editText.setText(data.getStringExtra("back"));
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/example/magshimim/ex8/ResultActivity.java
+++ app/src/main/java/com/example/magshimim/ex8/ResultActivity.java
@@ -1,7 +1,14 @@
 package com.example.magshimim.ex8;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 /**
  * Created by magshimim on 02/02/2018.
@@ -13,6 +20,53 @@ public class ResultActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_layout);
+        String far = getIntent().getStringExtra("far");
+        String cel = getIntent().getStringExtra("cel");
+        String check = getIntent().getStringExtra("check");
+        String returnValue;
+        TextView editText = (TextView) findViewById(R.id.answer);
+
+
+        Intent i = new Intent();
+        if(check.compareTo("rch") == 0)
+        {
+            Double Ifar = Double.parseDouble(far);
+            Double Icel= Double.parseDouble (cel);
+
+            if(Ifar == Icel*(9.0/5.0) + 32.0)
+            {
+                editText.setText("Bravo! the temperature " + cel + " ℃, is indeed " + far + "℉");
+
+            }
+            else
+            {
+                editText.setText("Oops! your answer is wrong, you may try again");
+            }
+        }
+        else
+        {
+            if(cel.length() > 0)
+            {
+                Double Icel= Double.parseDouble(cel);
+                editText.setText("the temperature " + cel + " ℃, is convert " + (Icel*(9.0/5.0) + 32.0) + "℉");
+                i.putExtra("back",""+ ((Icel*(9.0/5.0) + 32.0)));
+                i.putExtra("empty","far");
+                setResult(RESULT_OK, i);
+            }
+            else
+            {
+                Double Ifar = Double.parseDouble(far);
+                editText.setText("the temperature " + far + " ℉, is convert " + ((Ifar-32.0)*(5.0/9.0)) + "℃");
+                i.putExtra("back", ""+((Ifar-32.0)*(5.0/9.0)));
+                i.putExtra("empty","cel");
+                setResult(RESULT_OK, i);
+            }
+        }
+
+    }
+    public void goBack(View view)
+    {
+        finish();
     }
 }
 
